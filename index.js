9609bc3cdba140718de27ffb83f76dea2f67c3c4
tiotdev <tiotdev@tiot.de>
2019-08-05 19:37:26 +0200

    Send tokens async to handle 3 second blockchain limit on custom_json ops

--- index.js
+++ index.js
@@ -7,22 +7,34 @@ const path = require('path').dirname(require.main.filename);
 const ssc = new SSC('https://api.steem-engine.com/rpc');
 
 
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+
 function send(){
     if (config.mode.toLowerCase() != "issue" && config.mode.toLowerCase() != "transfer"){
         callback("Please only use issue or transfer in the mode in config.")
         return;
     }
     getTokens(result => {
-        result.forEach(element => {
+        asyncForEach(result, async element => {
             var sendJSON = {"contractName":"tokens","contractAction":config.mode.toLowerCase() ,"contractPayload":{"symbol": config.tokenSymbol,"to": element.author,"quantity": element.amountToSend,"memo":"Test"}}
-            steem.broadcast.customJson(config.accountPrivateActiveKey, [config.accountName], null, "ssc-mainnet1", JSON.stringify(sendJSON), function(err, result) {
+            await steem.broadcast.customJson(config.accountPrivateActiveKey, [config.accountName], null, "ssc-mainnet1", JSON.stringify(sendJSON), function(err, result) {
                 if (!err){
                     console.log(`Sent ${element.amountToSend} to ${element.author}.`)
                 } else {
                     console.log(`Error sending ${element.amountToSend} to ${element.author}.`)
                 }
-            });
-        });
+            })
+            await sleep(3500);
+          })
     })
 }
 
